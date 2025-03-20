b9e13c1c44ad61986ae933a69792dccdb379cca4
onikageyoshi <davidonyekachi29@email.com>
2025-03-21 01:23:46 +0300

    email sent

--- core/settings.py
+++ core/settings.py
@@ -292,8 +292,8 @@ settings.configure(
 send_mail(
     'Test Email',
     'This is a test email from Django.',
-    'davidonyekachi29@gmail.com',  # Sender
-    ['ore@gmail.com'],  # Receiver
+    'ore@gmail.com',  # Sender
+    ['davidonyekachi29@gmail.com'],  # Receiver
     fail_silently=False,
 )
 
