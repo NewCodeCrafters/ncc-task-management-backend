c8c13ddc66d1a507faa78d233355bd0e4d320673
onikageyoshi <davidonyekachi29@email.com>
2025-03-22 04:10:51 +0300

    added members to the stuff

--- core/urls.py
+++ core/urls.py
@@ -34,7 +34,8 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('members/', include('members.urls')),
     
     
 ]
