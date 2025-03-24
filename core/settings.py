c41ade40d5aa26d58d65f34159793676536418f8
onikageyoshi <davidonyekachi29@email.com>
2025-03-24 04:46:01 +0300

    otp finalised

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["ncc-task-management-backend.onrender.com", "localhost", "127.0.0.1"]
 database_url = os.environ.get("DATABASE_URL")
 
 
